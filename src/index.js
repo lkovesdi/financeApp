3ce9aee9ec5ceefa2268c74904eda68b6271b82e
Laszlo Kovesdi <lacikovesdi@gmail.com>
2019-01-19 16:09:29 -0800

    login form set up

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import configureStore from './store'
-
 // Styles
 
 
@@ -21,7 +19,11 @@ import configureStore from './store'
 
 
 // create react app version
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+    <App />,
+
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
